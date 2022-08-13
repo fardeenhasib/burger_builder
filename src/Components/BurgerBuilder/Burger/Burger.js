86ab22ca8fc281be478f4cb406d313d690262c0a
fardeenhasib <fardeenmozumder@gmail.com>
2022-08-13 01:41:01 -0400

    ingredients component working with flexible ingredients amount

--- src/Components/BurgerBuilder/Burger/Burger.js
+++ src/Components/BurgerBuilder/Burger/Burger.js
@@ -2,12 +2,18 @@ import React from "react";
 import Ingredients from "../Ingredients/Ingredients";
 
 const Burger = props => {
+    console.log(props);
+    let ingredientsArr = props.ingredients.map(item => {
+        let amountArr = [...Array(item.amount).keys()];
+        return amountArr.map(_ => {
+            return <Ingredients type = {item.type} />
+        })
+    })
+
     return (
         <div>
             <Ingredients type="bread-top" />
-            <Ingredients type="meat" />
-            <Ingredients type="cheese" />
-            <Ingredients type="salad" />
+            { ingredientsArr }
             <Ingredients type="bread-bottom" />
         </div>
     );
--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import Burger from "./Burger/Burger";
-import Ingredients from "./Ingredients/Ingredients";
+
 
 export default class BurgerBuilder extends Component{
+
+    state = {
+        ingredients: [
+            {type:'salad', amount:1 },
+            {type:'cheese', amount:1 },
+            {type:'meat', amount:1 }
+        ]
+    }
+
     render (){
         return (
             <div>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
             </div>
         );
     }
